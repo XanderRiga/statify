b151ed612345ed9211142f6c92285b08be22ebcc
Xander <xanderriga@live.com>
2019-08-17 16:26:53 -0700

    Add authenticate to the applcation
    Rspotify docs told me to do this, so here we are.

--- config/application.rb
+++ config/application.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
-
 require_relative 'boot'
-
 require 'rails/all'
+
+RSpotify::authenticate(ENV['SPOTIFY_CLIENT_ID'], ENV['SPOTIFY_CLIENT_SECRET'])
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.

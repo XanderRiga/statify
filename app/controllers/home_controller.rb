2a3605cff784fb618edce09a858cb05e73c0acef
Xander <xanderriga@live.com>
2019-11-03 21:14:42 -0800

    Stop user from entering landing page without linked acct

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,10 @@ class HomeController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    user_hash = SpotifyUser.find_by(user_id: current_user.id).spotify_user_hash
+    user = SpotifyUser.find_by(user_id: current_user.id)
+    return render 'home/index' unless user
+
+    user_hash = user.spotify_user_hash
     @spotify_user = RSpotify::User.new(user_hash)
   end
 end

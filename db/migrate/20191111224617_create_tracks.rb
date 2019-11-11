817b640bb4af262b2caee1ff23297caa412d37b4
Xander <xanderriga@live.com>
2019-11-11 14:48:43 -0800

    Create tracks table
    This will be part of how we keep track of every track a user has
    listened to. It is gonna get reaaaaal big.

---
+++ db/migrate/20191111224617_create_tracks.rb
@@ -0,0 +1,9 @@
+class CreateTracks < ActiveRecord::Migration[6.0]
+  def change
+    create_table :tracks do |t|
+      t.string :name
+      t.string :artist_name
+      t.jsonb :track
+    end
+  end
+end

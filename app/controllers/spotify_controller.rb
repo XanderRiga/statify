6c07d6bfdecc7f084fd106ac17c75bef6cdefacd
Xander <xanderriga@live.com>
2019-12-05 21:09:05 -0800

    Build recommendations page
    This finishes the absolute basics of the recommendation page. The known
    issues are: you need to select at least one artist or it fails, but
    there is no validation on the form. Once you get a recommendation, you
    cannot refresh the page.

--- app/controllers/spotify_controller.rb
+++ app/controllers/spotify_controller.rb
@@ -18,5 +18,35 @@ class SpotifyController < ApplicationController
   end
 
   def recommendation_result
+    @tracks = RSpotify::Recommendations.generate(formatted_form_response).tracks
+    # render 'spotify/recommendation_result'
+  end
+
+  private
+
+  def formatted_form_response
+    {
+        target_acousticness: params['acousticness'],
+        target_danceability: params['danceability'],
+        target_energy: params['energy'],
+        target_instrumentalness: params['instrumentalness'],
+        target_liveness: params['liveness'],
+        target_loudness: params['loudness'],
+        target_popularity: params['popularity'],
+        target_speechiness: params['speechiness'],
+        target_valence: params['valence'],
+        seed_artists: artist_list
+    }
+  end
+
+  def artist_list
+    artists = []
+    params.each do |key, value|
+      if key.include?('artist_')
+        artists << value
+      end
+    end
+
+    artists
   end
 end

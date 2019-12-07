36aa256d4e73d81719a1cdd597bd17d9d168b035
Xander <xanderriga@live.com>
2019-12-07 11:57:17 -0800

    Validate recommendation form data
    This gives some basic validaiton and UX stuff. We show a loading bar
    when a change is happening so users can see it, is they select no
    artists we tell them they need at least 1 and we don't allow them to
    click more than 5 artists at all.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,7 +3,13 @@
 $(document).ready(function() {
     $(":input").each(function() {
         $(this).change(function() {
-            update_recommendations();
+            if (too_many_checked()) {
+                alert("You can only select up to 5 artists");
+                this.checked = false;
+            } else {
+                show_progress_bar();
+                update_recommendations();
+            }
         });
     });
 });
@@ -14,14 +20,15 @@ function update_recommendations() {
         type: 'get',
         dataType: 'json',
         data: data_from_inputs(),
-        success: function(data) { set_data(data) }
+        success: function(data) { set_data(data) },
+        error: function(error) { handle_error(error) }
     });
 }
 
 function data_from_inputs() {
-    data = {};
+    let data = {};
     $(":input").each(function() {
-        input = $(this);
+        let input = $(this);
 
         if (input.attr('type') === 'checkbox') {
             if (input.prop('checked') === true) {
@@ -43,6 +50,26 @@ function set_data(data) {
         let row = '<tr><td>' + track.name + '</td><td>' + track.artists.join(', ') + '</td><td>' + track.album + '</td></tr>';
 
         $('#help-text').hide();
+        hide_progress_bar();
         tableBody.append(row);
     }
+    $('#recommendation-table').show();
+}
+
+function show_progress_bar() {
+    $('#progress-bar').removeClass('is-hidden');
+}
+
+function hide_progress_bar() {
+    $('#progress-bar').addClass('is-hidden');
+}
+
+function handle_error(error) {
+    hide_progress_bar();
+    $('#help-text').show();
+    $('#recommendation-table').hide();
+}
+
+function too_many_checked() {
+    return ($('input[type=checkbox]:checked').length > 5)
 }

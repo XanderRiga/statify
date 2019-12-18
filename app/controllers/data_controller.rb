9c800bf3981b8a5001f81a5402dc58be3be8653e
Xander <xanderriga@live.com>
2019-12-18 15:46:09 -0800

    Revert "Move and rename jobs"
    This reverts commit f489377fb63beb9f5edee56c4c791a3d9be492ec.

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -26,7 +26,7 @@ class DataController < ApplicationController
           file_path: file_path
         )
 
-        StreamingHistory::SaveFileJob.perform_async(steaming_history.id)
+        SaveStreamingHistoryJob.perform_async(steaming_history.id)
       end
     end
 

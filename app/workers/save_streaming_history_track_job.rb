283eb547849baf05cf28974e748dcfb8b676d028
Xander <xanderriga@live.com>
2019-12-18 15:46:09 -0800

    Revert "Move and rename jobs"
    This reverts commit f489377fb63beb9f5edee56c4c791a3d9be492ec.

---
+++ app/workers/save_streaming_history_track_job.rb
@@ -0,0 +1,20 @@
+require 'rspotify'
+
+class SaveStreamingHistoryTrackJob
+  include Sidekiq::Worker
+
+  def perform(json_listen, user_id)
+    listen = JSON.parse(json_listen)
+
+    track = RSpotify::Track.search(listen['trackName'] + ' ' + listen['artistName']).first
+    Scrobble.create(
+      track_id: track.id,
+      created_at: listen['endTime'],
+      user_id: user_id,
+      updated_at: listen['endTime'],
+      artist_ids: track.artists.map(&:id),
+      track_name: listen['trackName'],
+      artist_name: listen['artistName']
+    ) if track
+  end
+end

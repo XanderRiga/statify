d4f30c448f39e697277af9970c95346fc93e146d
Xander <xanderriga@live.com>
2019-12-19 12:55:41 -0800

    Start using some stats from the DB
    This is the most basic search I can do, this will show a users top all
    time artists on the home page from the BD instead of from a query to
    spotify.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,20 +1,30 @@
 # frozen_string_literal: true
-
 require 'users/helpers/retrieve_spotify_user'
+require 'statistics/artists'
 
 class HomeController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @spotify_user = Users::Helpers::RetrieveSpotifyUser.new.call(user_id: current_user.id)
-
-    render :show if @spotify_user
+    if spotify_user
+      @top_artists = Statistics::Artists.top(user_id: current_user.id).first(10)
+      @recently_played = spotify_user.recently_played(limit: 10)
+      render :show
+    end
   rescue Users::Exceptions::UserNotFound
     render 'home/index'
   end
 
   def show
-    @spotify_user = Users::Helpers::RetrieveSpotifyUser.new.call(user_id: current_user.id)
-    render 'home/index' unless @spotify_user
+    @top_artists = Statistics::Artists.top(user_id: current_user.id).first(10)
+    @recently_played = spotify_user.recently_played(limit: 10)
+
+    render 'home/index' unless spotify_user
+  end
+
+  private
+
+  def spotify_user
+    @spotify_user ||= Users::Helpers::RetrieveSpotifyUser.new.call(user_id: current_user.id)
   end
 end

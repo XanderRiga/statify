127903d94ded1b942cb3fdc38b440ad9e7da4a27
Xander <xanderriga@live.com>
2020-01-01 09:57:02 -0800

    Update Artists and add Tracks top
    The logic in here was kinda bad so I patched it up and copied it over to
    a top tracks. There is definitely some potential to pull from a common
    parent here, but I just can't be bothered.

--- lib/statistics/tracks.rb
+++ lib/statistics/tracks.rb
@@ -1,23 +1,23 @@
 module Statistics
-  class Artists
+  class Tracks
     def self.top(user_id:, start_date: Time.parse('2006-04-23'), end_date: Time.now)
       scrobbles = Scrobble.in_date_range(start_date, end_date).where(user_id: user_id)
 
       scrobbles.each_with_object(Hash.new({})) do |scrobble, hash|
-        if valid_artist(scrobble.artist_name)
-          if hash[scrobble.artist_name] == {}
-            hash[scrobble.artist_name] = {
+        if valid_track(scrobble.track_name)
+          if hash[scrobble.track_name] == {}
+            hash[scrobble.track_name] = {
                 'occurrences' => 0,
-                'id' => scrobble.artist_ids.first
+                'id' => scrobble.track_id
             }
           end
-          hash[scrobble.artist_name]['occurrences'] += 1
+          hash[scrobble.track_name]['occurrences'] += 1
         end
       end.sort_by{ |k,v| v['occurrences'] }.reverse!
     end
 
-    def self.valid_artist(artist_name)
-      artist_name && artist_name != 'Unknown Artist'
+    def self.valid_track(track_name)
+      track_name && track_name != 'Unknown Track'
     end
   end
 end

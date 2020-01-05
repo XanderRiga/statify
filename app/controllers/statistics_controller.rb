a29312b7a70f4fd2dac9d4a3bf4aa15efac0f26a
Xander <xanderriga@live.com>
2020-01-05 10:56:33 -0800

    Add all time top tracks and artists to overview
    I am running out of ideas for this page despite having barely anything
    since the API limits me, along with my own DB design. There are no
    genres for tracks in spotify which makes it very hard to come up with
    more charts worth showing. For now this will have to fill some space.

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -18,7 +18,7 @@ class StatisticsController < ApplicationController
   end
 
   def top_artists
-    return render json: {}, status: 400 if params['start_date'].empty? || params['end_date'].empty?
+    return render json: {}, status: 400 if params['end_date'].empty?
     top_artists = Statistics::Artists.top(user_id: current_user.id, start_date: params['start_date'], end_date: params['end_date'])
 
     render json: top_artists.first(10), status: 200
@@ -28,7 +28,7 @@ class StatisticsController < ApplicationController
   end
 
   def top_tracks
-    return render json: {}, status: 400 if params['start_date'].empty? || params['end_date'].empty?
+    return render json: {}, status: 400 if params['end_date'].empty?
     top_artists = Statistics::Tracks.top(user_id: current_user.id, start_date: params['start_date'], end_date: params['end_date'])
 
     render json: top_artists.first(10), status: 200

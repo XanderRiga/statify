45d4b136515bebcdc02db12a9432e9f93adb3b3e
Xander <xanderriga@live.com>
2020-04-28 21:47:42 -0700

    Use proper active record queries
    This is what I wanted to do in the first place, but my garbage DB didn't
    let me. This should allow for more powerful queries without writing raw
    SQL.

--- lib/statistics/hears.rb
+++ lib/statistics/hears.rb
@@ -1,11 +1,7 @@
 module Statistics
   class Hears
     def self.explicit_hash(user_id:)
-      sql = 'select count(*)'\
-        ' from hears'\
-        ' inner join tracks on hears.track_id = tracks.id'\
-        " where user_id = #{user_id} and tracks.explicit = true"
-      num_explicit = ActiveRecord::Base.connection.execute(sql).first['count']
+      num_explicit = Hear.where(user_id: user_id, track: Track.where(explicit: true)).count
 
       {
         'Explicit' => num_explicit,
@@ -14,25 +10,21 @@ module Statistics
     end
 
     def self.popular_listens_hash(user_id:)
-      num_most_popular = popularity_count(user_id: user_id, lower_bound: 75, upper_bound: 100)
+      num_most_popular = popularity_count(user_id: user_id, lower_bound: 75, upper_bound: 101)
       num_somewhat_popular = popularity_count(user_id: user_id, lower_bound: 50, upper_bound: 75)
       num_less_popular = popularity_count(user_id: user_id, lower_bound: 25, upper_bound: 50)
       num_unpopular = popularity_count(user_id: user_id, lower_bound: 0, upper_bound: 25)
 
       {
         'Most Popular(75-100)' => num_most_popular,
-        'Somewhat Popular(50-75)' => num_somewhat_popular,
-        'Less Popular(25-50)' => num_less_popular,
-        'Unopular(0-25)' => num_unpopular
+        'Somewhat Popular(50-74)' => num_somewhat_popular,
+        'Less Popular(25-49)' => num_less_popular,
+        'Unopular(0-24)' => num_unpopular
       }
     end
 
     def self.popularity_count(user_id:, lower_bound:, upper_bound:)
-        sql = 'select count(*)'\
-        ' from hears'\
-        ' inner join tracks on hears.track_id = tracks.id'\
-        " where user_id = #{user_id} and cast(tracks.popularity as int) < #{upper_bound} and cast(tracks.popularity as int) > #{lower_bound}"
-      ActiveRecord::Base.connection.execute(sql).first['count']
+      Hear.joins(:track).where(user_id: user_id).where("cast(tracks.popularity as int) < #{upper_bound} and cast(tracks.popularity as int) >= #{lower_bound}").count
     end
 
     def self.num_all_hears(user_id:)

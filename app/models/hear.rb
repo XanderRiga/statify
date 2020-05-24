aa8dc0151184d748c2ca52e1fdb03d8fe5a20a93
Xander <xanderriga@live.com>
2020-05-23 20:13:25 -0700

    Add top 5 artists of last 6 months
    This needs a name rewrite for sure, but this puts in the basics of a
    chart I want to implement. It is sort of impossible to test manually at
    the moment since my DB is basically empty, but I _think_ it works, and
    when I use it for a chart for real I will make sure I actually have data
    to try it with.

--- app/models/hear.rb
+++ app/models/hear.rb
@@ -1,3 +1,5 @@
 class Hear < ApplicationRecord
   belongs_to :track
+
+  scope :in_date_range, ->(start_date, end_date) { where(created_at: start_date.to_date.beginning_of_day..end_date.to_date.end_of_day) }
 end
--- lib/statistics/artists.rb
+++ lib/statistics/artists.rb
@@ -1,23 +1,43 @@
 module Statistics
   class Artists
     def self.top(user_id:, start_date: Time.parse('2006-04-23'), end_date: Time.now)
-      scrobbles = Scrobble.in_date_range(start_date, end_date).where(user_id: user_id)
+      hears = Hear.in_date_range(start_date, end_date).where(user_id: user_id)
 
-      scrobbles.each_with_object(Hash.new({})) do |scrobble, hash|
-        if valid_artist(scrobble.artist_name)
-          if hash[scrobble.artist_name] == {}
-            hash[scrobble.artist_name] = {
+      hears.each_with_object(Hash.new({})) do |hear, hash|
+        artist_name = hear.track.artists.first.name
+        if valid_artist(artist_name)
+          if hash[artist_name] == {}
+            hash[artist_name] = {
                 'occurrences' => 0,
-                'id' => scrobble.artist_ids.first
+                'id' => hear.track.artists.first.id
             }
           end
-          hash[scrobble.artist_name]['occurrences'] += 1
+          hash[artist_name]['occurrences'] += 1
         end
       end.sort_by{ |k,v| v['occurrences'] }.reverse!
     end
 
     def self.valid_artist(artist_name)
       artist_name && artist_name != 'Unknown Artist'
+    end
+
+    def self.top_5_last_6_months(user_id:)
+      start_date = Time.now.beginning_of_month
+      end_date = Time.now
+
+      top_artists = []
+      6.times do
+        top_artists << self.top(
+            user_id: user_id,
+            start_date: start_date,
+            end_date: end_date
+        ).slice(0, 4)
+
+        start_date = start_date.last_month
+        end_date = end_date.last_month
+      end
+
+      top_artists
     end
   end
 end

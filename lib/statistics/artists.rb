0f6d8cb69bbabb3c501bfd21bf83aae5da27f505
Xander <xanderriga@live.com>
2020-05-24 21:00:19 -0700

    Massively improve top artists query
    This should have been a query all along and now that the tables are
    actually set up correctly, we can use this query. This improves
    performance by roughly a billion by my calculations.

--- lib/statistics/artists.rb
+++ lib/statistics/artists.rb
@@ -1,20 +1,13 @@
 module Statistics
   class Artists
     def self.top(user_id:, start_date: Time.parse('2006-04-23'), end_date: Time.now)
-      hears = Hear.in_date_range(start_date, end_date).where(user_id: user_id)
-
-      hears.each_with_object(Hash.new({})) do |hear, hash|
-        artist_name = hear.track.artists.first.name
-        if valid_artist(artist_name)
-          if hash[artist_name] == {}
-            hash[artist_name] = {
-                'occurrences' => 0,
-                'id' => hear.track.artists.first.id
-            }
-          end
-          hash[artist_name]['occurrences'] += 1
-        end
-      end.sort_by{ |k,v| v['occurrences'] }.reverse!
+      Hear.where(user_id: user_id)
+          .in_date_range(start_date, end_date)
+          .joins(track: [:artists])
+          .group('artists.name')
+          .order(count_all: :desc)
+          .limit(10)
+          .count
     end
 
     def self.valid_artist(artist_name)

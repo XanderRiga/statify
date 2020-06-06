350944b755c63d6c4b5b5232297107ed4e4013ca
Xander <xanderriga@live.com>
2020-06-06 11:26:18 -0700

    Trying more mailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -115,7 +115,6 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
       address: "smtp.gmail.com",
       port: 587,
-      domain: "example.com",
       authentication: "plain",
       enable_starttls_auto: true,
       user_name: ENV["GMAIL_USERNAME"],

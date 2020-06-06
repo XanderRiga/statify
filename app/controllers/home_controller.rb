8fb4d8b52fb868a989846af9b020a579cf9c60fa
Xander <xanderriga@live.com>
2020-06-06 13:53:56 -0700

    Update home page
    The photo was kind of a joke since it changes a lot and I need a user to
    login every time to get the updated on, so screw that. Also, this page
    was just ugly as hell. This makes it slightly better, and also uses DB
    data instead of hitting the network if we can.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -18,7 +18,19 @@ class HomeController < ApplicationController
 
   def show
     @spotify_user = Users::Helpers::RetrieveSpotifyUser.new.call(user_id: current_user.id)
-    @recently_played = @spotify_user.recently_played(limit: 10)
+
+    recently_played_db = Hear
+                             .where(user_id: current_user.id)
+                             .order(created_at: :asc)
+                             .last(10)
+
+    if recently_played_db
+      @recently_played = recently_played_db.reverse.map do |hear|
+        hear.track
+      end
+    else
+      @recently_played = @spotify_user.recently_played(limit: 10)
+    end
 
     render 'home/index' unless @spotify_user
   end
